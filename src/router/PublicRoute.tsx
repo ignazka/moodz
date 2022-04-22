c9fae9981bd33e7baa35dad9cd267baf8b9ccd67
Philipp Kastl <picastle@posteo.de>
2022-04-22 18:10:13 +0200

    working firebase integration

---
+++ src/router/PublicRoute.tsx
@@ -0,0 +1,20 @@
+import { useLocation, Navigate } from 'react-router-dom';
+import useAuth from '../context/authContext';
+import { ROUTES } from './constants';
+
+function PublicRoute({ children }: { children: JSX.Element }) {
+  let { user } = useAuth();
+  let location = useLocation();
+
+  if (user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to={ROUTES.main} state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+export default PublicRoute;
