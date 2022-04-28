2cf70c73864f06aca1fb7451ffa0ca477dc4bcfa
Evij <=>
2022-04-29 01:17:02 +0200

    remove unused

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import useAuth from '../context/authContext';
 
 import { BottomNavigation, BottomNavigationAction} from '@mui/material';
