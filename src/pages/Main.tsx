b259e79863e7e563ed619db25f6df3c184d72131
Philipp Kastl <picastle@posteo.de>
2022-05-15 10:38:12 +0200

    theming

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -23,38 +23,38 @@ const Main = (props: any): any => {
   const [moodz, setMoodz] = useState<any | null>([{}]);
 
 
-  const theme = createTheme({
-    palette: {
-      mode: 'dark',
-      //mode: 'light',
-      primary: {
-        // light: will be calculated from palette.primary.main,
-        main: '#ace6ac',
-        // #b99747 dark: will be calculated from palette.primary.main,
-        // contrastText: will be calculated to contrast with palette.primary.main
-      },
-      background: {
-        default: '#ace6ac',
+  // const theme = createTheme({
+  //   palette: {
+  //     mode: 'dark',
+  //     //mode: 'light',
+  //     primary: {
+  //       // light: will be calculated from palette.primary.main,
+  //       main: '#ace6ac',
+  //       // #b99747 dark: will be calculated from palette.primary.main,
+  //       // contrastText: will be calculated to contrast with palette.primary.main
+  //     },
+  //     background: {
+  //       default: '#ace6ac',
 
-      },
-      secondary: {
-        main: '#ffd7a3',
-        // #0044ff dark: will be calculated from palette.secondary.main,
-        contrastText: '#2c2c2c',
-      },
-      // Used by `getContrastText()` to maximize the contrast between
-      // the background and the text.
-      contrastThreshold: 3,
-      // Used by the functions below to shift a color's luminance by approximately
-      // two indexes within its tonal palette.
-      // E.g., shift from Red 500 to Red 300 or Red 700.
-      tonalOffset: 0.2,
-
-
-    },
+  //     },
+  //     secondary: {
+  //       main: '#ffd7a3',
+  //       // #0044ff dark: will be calculated from palette.secondary.main,
+  //       contrastText: '#2c2c2c',
+  //     },
+  //     // Used by `getContrastText()` to maximize the contrast between
+  //     // the background and the text.
+  //     contrastThreshold: 3,
+  //     // Used by the functions below to shift a color's luminance by approximately
+  //     // two indexes within its tonal palette.
+  //     // E.g., shift from Red 500 to Red 300 or Red 700.
+  //     tonalOffset: 0.2,
 
 
-  });
+  //   },
+
+
+  // });
 
   /**
    * send data to firebase
@@ -121,7 +121,7 @@ const Main = (props: any): any => {
   }, []);
 
   return (
-    <ThemeProvider theme={props.theme}
+    <ThemeProvider theme={theme}
     >
       <div className=''>
         <AppBar className="appbar" color="inherit">
