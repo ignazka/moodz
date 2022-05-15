92267a7623525409967d99dfd00b1906881099a4
Philipp Kastl <picastle@posteo.de>
2022-05-15 20:40:36 +0200

    add toggle for light and dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import { useState } from 'react';
 import BottomNav from './components/BottomNav';
 import Header from './components/Header';
 import { AppRouter } from './router';
-
+import { themes } from './utils/theme'
+import { useRecoilState } from 'recoil'
+import { themeValue } from './atoms/settingsAtom'
 
 
 function App() {
   const [settings, setSettings] = useState<any | null>([{}]);
-
+  const [theme] = useRecoilState(themeValue)
   const handleSettingsChange = (props: any) => {
     const { name, value } = props;
     // console.log("inputTerm", { ...inputTerm, [name]: value });
@@ -17,16 +20,21 @@ function App() {
 
 
   return (
-    <div>
-      <Header />
+    <ThemeProvider theme={themes[theme]}>
 
-      <AppRouter
-        settings={settings}
-        handleSettingsChange={handleSettingsChange}
+      <div>
 
-      />
-      <BottomNav />
-    </div>
+
+        <Header />
+
+        <AppRouter
+          settings={settings}
+          handleSettingsChange={handleSettingsChange}
+
+        />
+        <BottomNav />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,69 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { AppBar, Typography } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-//components
-import SettingsCard from '../components/FormCard';
-// import Moodchart from '../components/Moodchart';
-import BottomNav from '../components/BottomNav';
-// import { useFetch } from '../hooks/useFetch';
+import { Card, FormControl, FormControlLabel, FormLabel, Paper, Radio, RadioGroup, Typography } from '@mui/material';
+import { useRecoilState } from 'recoil'
+import { themeValue } from '../atoms/settingsAtom'
 
 
 
+const Settings = (props: any): any => {
 
-const Settings= (props: any): any => {
+    const [theme, setTheme] = useRecoilState(themeValue)
 
-
-    // const { data, loading, error } = useFetch(() => { }, []);
-
-   
-
-
-    
-    // // form handler
-
-    //   const handleInputChange = (props: any) => {
-    //     const { name, value } = props;
-    //     // console.log("inputTerm", { ...inputTerm, [name]: value });
-    //     setSettings({ ...settings, [name]: value });
-    //   };
-
-
-    //   useEffect(() => {
-
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    //   }, []);
-
+    const handleChange = ({ target }: any) => {
+        switch (target.defaultValue) {
+            case 'dark':
+                setTheme(0)
+                break;
+            case 'light':
+                setTheme(1)
+                break;
+            default:
+        }
+    }
     return (
-        <ThemeProvider theme={props.theme}
-        >
-            <div className=''>
-                <AppBar className="appbar" color="inherit">
-                    <Typography className="apptitle" align="center" variant="h3">
-                        MOODZ
-                    </Typography>
+        <div className='Settings'>
+            <Card>
 
-                </AppBar>
+                <Paper sx={{ padding: '2em' }}>
 
+                    <FormControl>
+                        <FormLabel>Theme</FormLabel>
+                        <RadioGroup
+                            defaultValue="dark"
+                            name="radio-buttons-group"
+                            onChange={handleChange}
 
+                        >
+                            <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+                            <FormControlLabel value="light" control={<Radio />} label="Light" />
+                        </RadioGroup>
+                    </FormControl>
+                </Paper>
+            </Card>
+        </div >
 
-
-                {/* ------------- FORM -------------- */}
-
-
-
-                <SettingsCard
-                    handleInputChange={props.handleSettingsChange}
-                    style={{ marginTop: 50, margin: 15, padding: 0, marginBottom: 85 }}
-
-                />
-
-                <BottomNav
-                    setSettings={props.setSettings}
-                />
-
-            </div>
-        </ThemeProvider>
     );
 }
 
