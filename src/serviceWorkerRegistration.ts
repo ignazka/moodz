e2c8390b0915aa8168c1df239185532d5c1d5d0f
Evij <=>
2022-05-21 13:43:56 +0200

    register serviceworker

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -24,11 +24,7 @@ type Config = {
 };
 
 export function register(config?: Config) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for(let registration of registrations) {
-     registration.unregister()
-   } })
-   
+  
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
