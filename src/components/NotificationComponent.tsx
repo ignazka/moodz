52ccc5f6abbe105877a1a75896705f30d0473373
Philipp Kastl <picastle@posteo.de>
2022-05-27 20:37:31 +0200

    eslint disable

--- src/components/NotificationComponent.tsx
+++ src/components/NotificationComponent.tsx
@@ -17,7 +17,7 @@ function NotificationComponent() {
     const now = new Date()
     const eightOClock = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 0, 0, 0).getTime() - now.getTime();
     const twelveOClock = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0, 0, 0).getTime() - now.getTime();
-    const sixOClockPM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 37, 0, 0).getTime() - now.getTime();
+    const sixOClockPM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 40, 0, 0).getTime() - now.getTime();
 
 
     useEffect(() => {
@@ -38,7 +38,6 @@ function NotificationComponent() {
 
         }
         // eslint-disable-next-line
-
     }, [notificationToggle])
 
 
