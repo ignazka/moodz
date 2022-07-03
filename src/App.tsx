a6ea7bc7f1b9e5c37c7dc37526ae0f686b8597bd
evij-g <evgenij@evgenijgottfried.de>
2022-07-03 12:28:20 +0200

    remove unused stuff and forward toggle value

--- src/App.tsx
+++ src/App.tsx
@@ -5,68 +5,67 @@ import Header from './components/Header';
 import { AppRouter } from './router';
 import { themes } from './utils/theme'
 import { useRecoilState } from 'recoil'
-import { themeValue, notificationToggle } from './atoms/settingsAtom'
+import { notificationToggle, themeValue } from './atoms/settingsAtom'
 
 
 
 function App() {
   // console.log(themeValue, useRecoilState(notificationToggle));
-  const [settings, setSettings] = useState<any | null>([{}]);
-  const [theme] = useRecoilState(themeValue)
-  const [notifRef] = useRecoilState(notificationToggle);
-  // setSettings({...settings,["notificationToggle"]:notifRef});
-  const updates = useRef([{updates:0,...settings}]);
+  // const [settings, setSettings] = 
+  
+  const [theme] = useRecoilState(themeValue);
+  const [notifToggle,setNotifToggle] = useRecoilState(notificationToggle);
+
+  // console.log("notifRef from SettingsAtom",notifRef); //funktioniert!
+  // console.log("settings",settings);
+  // const updates = useRef([{updates:0,...settings}]);
+
+
 
   
 
-  const handleSettingsChange = (props: any) => {
-    console.log(props);
-    const { name, value } = props;
-    // console.log("inputTerm", { ...inputTerm, [name]: value });
-    setSettings({ ...settings, [name]: value });
-    console.log("settings change",settings);
-  };
 
 
- // function Child({ counter, currentTimer }) {
-  function Child(props:any) {
-    console.log("props",props);
-    console.log("settings child component", settings);
+//  // function Child({ counter, currentTimer }) {
+//   function Child(props:any) {
+//     console.log("props",props);
+//     // console.log("settings child component", settings);
 
-    // handleSettingsChange(props);
+//     // handleSettingsChange(props);
 
-    // this will clearInterval in parent component after counter gets to 5
-    // useEffect(() => {
-    //   //führe den clearInterval gar nicht erst aus, solange der counter nicht bei 5 ist
-    //   // if (counter <= 5) return; //it is a loop condition so to speak
-    //   // if (toggleValue) return; //it is a loop condition so to speak
-    //   //currentTimer is also updated because with every call the intervalID changes
-    //   // setCounter(0);
-    //   // clearInterval(currentTimer); //currentTimer stores the intervalID
-    //   //counter is the number which counts up with each call
-    //   //}, [toggleRef, currentTimer]); change in toggleRef doesn't force a re-render
-    //   //instead we need observe the toggleValue state change
+//     // this will clearInterval in parent component after counter gets to 5
+//     // useEffect(() => {
+//     //   //führe den clearInterval gar nicht erst aus, solange der counter nicht bei 5 ist
+//     //   // if (counter <= 5) return; //it is a loop condition so to speak
+//     //   // if (toggleValue) return; //it is a loop condition so to speak
+//     //   //currentTimer is also updated because with every call the intervalID changes
+//     //   // setCounter(0);
+//     //   // clearInterval(currentTimer); //currentTimer stores the intervalID
+//     //   //counter is the number which counts up with each call
+//     //   //}, [toggleRef, currentTimer]); change in toggleRef doesn't force a re-render
+//     //   //instead we need observe the toggleValue state change
 
       
-    // });
-    // 
+//     // });
+//     // 
 
-    return null;
-  }
+//     return null;
+//   }
 
   return (
     <ThemeProvider theme={themes[theme]}>
       <div>
         <Header />
         <AppRouter
-          settings={settings}
-          notificationToggle={notifRef}
-          handleSettingsChange={handleSettingsChange}
-          updates={updates}
+          // settings={settings}
+           notificationToggle={notifToggle}
+           setNotificationToggle={setNotifToggle}
+          // handleSettingsChange={handleSettingsChange}
+          // updates={updates}
         />
          {/* <Child
         // props={[notifRef]}
-        notificationToggle={notifRef}
+        // notificationToggle={notifRef}
       /> */}
         <BottomNav />
       </div>
