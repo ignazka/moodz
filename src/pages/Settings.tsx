f5955c3e8303ac02a035cf27e713fc14c25fb062
Evij <=>
2022-07-16 11:30:08 +0200

    add local storage #56

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,6 +1,6 @@
 import { Card } from '@mui/material';
 import { useRecoilState } from 'recoil';
-import { notificationTimes,timer } from '../atoms/settingsAtom';
+import { notificationTimes, timer, notificationToggle } from '../atoms/settingsAtom';
 
 import NotificationComponent from '../components/NotificationComponent';
 import Theme from '../components/Theme';
@@ -8,68 +8,113 @@ import Theme from '../components/Theme';
 
 
 const Settings = (props: any): any => {
-    
-     const [notifTimes,setNotifTimes] = useRecoilState(notificationTimes);
-     const [intervalTimer,setIntervalTimer] = useRecoilState(timer);
 
-    let currentNotificationTimes;
+    const [notifTimes, setNotifTimes] = useRecoilState(notificationTimes);
+    const [intervalTimer, setIntervalTimer] = useRecoilState(timer);
+    const [notifToggle, setNotifToggle] = useRecoilState(notificationToggle);
+
+    let currentNotificationTimes = notifTimes;
     let currentIntervalTimer;
-    let currentNotificationToggle;
+    let currentNotificationToggle: any = notifToggle;
 
     // const settings = useRef({notificationToggle:notifRef});
 
     // console.log({...props});
-    let val = {...props};
-    val=val.props;
+    let val = { ...props };
+    val = val.props;
     // console.log("val",val);
-    console.log("...notifTimes",{...notifTimes});
-    console.log("intervalTimer",intervalTimer);
+    console.log("...notifTimes", { ...notifTimes });
+    console.log("intervalTimer", intervalTimer);
+    console.log("notifToggle", notifToggle);
 
+    // console.log("##### LOCAL STORAGE: setSettings() #####");
+    // setSettings();
+    if (localStorage.getItem('notificationToggle')) {
+        if (localStorage.getItem('notificationToggle')?.valueOf() === null) {
+            // console.log("##### LOCAL STORAGE: variable noch vorhanden #####", localStorage.getItem('notificationToggle')?.valueOf());
+            // setNotifToggle(false);
+            console.warn("local storage === null");
+        }
+        else {
+            console.log("local storage not defined!");
 
-    if(localStorage.getItem('notificationToggle')) {
+            // setNotifToggle(localStorage.getItem('notificationToggle') === "true" ? true : false);
+            updateSettings();
+        }
+        console.log('notifToggle '+notifToggle + "localToggle " + localStorage.getItem('notificationToggle'));
         
-        setSettings();
+        // console.log("##### LOCAL STORAGE: variable toggle #####", localStorage.getItem('notificationToggle'));
+        
+        // currentNotificationToggle=(notifToggle);
 
-      } else {
-        updateSettings();
-      }
-      
-      function updateSettings() {
+    } else {
+        console.log("##### LOCAL STORAGE: setSettings() #####");
+        setSettings();
+    }
+
+    function updateSettings() {
         console.log("set local storage");
-        localStorage.setItem('notificationTimes', JSON.stringify({...notifTimes}));
+        localStorage.setItem('notificationTimes', JSON.stringify({ ...notifTimes }));
         localStorage.setItem('intervalTimer', intervalTimer.toString());
-        let nT = !val.notificationToggle;
-        localStorage.setItem('notificationToggle', nT.toString());
+        currentNotificationToggle = (notifToggle);
+
+        localStorage.setItem('notificationToggle', currentNotificationToggle.toString());
         // localStorage.setItem('image', document.getElementById('image').value);
-      
-       // setSettings();
-      }
-      
-      function setSettings() {
-      
-      
-        localStorage.setItem('notificationTimes', JSON.stringify({...notifTimes}));
-        localStorage.setItem('intervalTimer', intervalTimer.toString());
-        localStorage.setItem('notificationToggle', val.notificationToggle);
+
+        // setSettings();
 
         currentNotificationTimes = JSON.parse(localStorage.getItem('notificationTimes')!);
         currentIntervalTimer = localStorage.getItem('intervalTimer')?.valueOf();
-        currentNotificationToggle = localStorage.getItem('notificationToggle')?.valueOf();
+        //    currentNotificationToggle = (localStorage.getItem('notificationToggle') === "true" ? true:false);
+    }
 
-        console.log("currentNotificationTimes",currentNotificationTimes);
-        console.log("currentIntervalTimer",currentIntervalTimer);
-        console.log("currentNotificationToggle",currentNotificationToggle);
-      
-      }
-      
+    function setSettings() {
+
+        console.log("---set Settings---");
+
+        localStorage.setItem('notificationTimes', JSON.stringify({ ...notifTimes }));
+        localStorage.setItem('intervalTimer', intervalTimer.toString());
+        // localStorage.setItem('notificationToggle', notificationToggle.toString());
+
+        currentNotificationTimes = JSON.parse(localStorage.getItem('notificationTimes')!);
+        currentIntervalTimer = localStorage.getItem('intervalTimer')?.valueOf();
+        // currentNotificationToggle = localStorage.getItem('notificationToggle') === "true" ? true : false;
+
+        console.log("currentNotificationTimes", currentNotificationTimes);
+        console.log("currentIntervalTimer", currentIntervalTimer);
+        console.log("currentNotificationToggle", currentNotificationToggle);
+
+    }
+
+
+    const handleSettingsChange = (e: any) => {
+        console.log("setNotificationToggle", e);
+        // const { name, value } = test;
+        setNotifToggle(e);
+        updateSettings();
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-  const handleSettingsChange = (e: any) => {
-    console.log("setNotificationToggle",e);
-    // const { name, value } = test;
-    val.setNotificationToggle(e);
-    setSettings();
-  };
-
 
 
     return (
@@ -84,13 +129,13 @@ const Settings = (props: any): any => {
             <Card
                 style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', margin: 15, padding: 10, paddingTop: 20 }}>
                 <div className='settings-ctn'>
-                    <NotificationComponent 
-                    notificationToggle={val.notificationToggle}
-                    notificationTimes={currentNotificationTimes}
-                    setNotifTimes={setNotifTimes}
-                    intervalTimer={currentIntervalTimer}
-                    setIntervalTimer={setIntervalTimer} 
-                    handleSettingsChange={handleSettingsChange}
+                    <NotificationComponent
+                        notificationToggle={currentNotificationToggle}
+                        notificationTimes={currentNotificationTimes}
+                        setNotifTimes={setNotifTimes}
+                        intervalTimer={currentIntervalTimer}
+                        setIntervalTimer={setIntervalTimer}
+                        handleSettingsChange={handleSettingsChange}
                     />
                 </div>
             </Card >
