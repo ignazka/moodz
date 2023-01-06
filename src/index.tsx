bca54864ebfb73b3ba35e6df5cab7c3bc201988f
Evij <=>
2023-01-06 13:14:15 +0100

    register

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 
- serviceWorkerRegistration.unregister();
+ serviceWorkerRegistration.register();
 
 
 
